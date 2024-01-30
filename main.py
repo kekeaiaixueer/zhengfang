f5f6526163e1a258403e29a013569b2d6bfb532a
雪儿 <109696082+kekeaiaixueer@users.noreply.github.com>
2024-01-30 18:39:46 +0800

    Update main.py

--- main.py
+++ main.py
@@ -10,110 +10,12 @@ PASSWORD = os.environ.get("PASSWORD")         # 登录教务系统的密码
 YEAR = os.environ.get("YEAR")              # 要查询成绩的学年
 TERM = os.environ.get("TERM")                      # 要查询成绩的学期
 TOKEN = os.environ.get("TOKEN")  # PushPlus的令牌，用于发送通知
-
-proxies_list = [
-    {"http": "http://183.247.152.98:53281", "https": "https://183.247.152.98:53281"},
-    {"http": "http://117.26.41.218:8888", "https": "https://117.26.41.218:8888"},
-    {"http": "http://58.220.95.55:9400", "https": "https://58.220.95.55:9400"},
-    
-    # 更多代理...
-]
-
-DATA_FILE = 'data.txt'          # 存储当前数据的文件
-NEW_DATA_FILE = 'new_data.txt'  # 用于存储新数据以便比较的临时文件
-# 登录教务系统的函数
-def login_school(url, username, password, proxies):
-    school_client = SchoolClient(url,proxies=proxies)
-    return school_client.user_login(username, password)
-
-# 登录后从教务系统获取个人信息的函数
-# def get_personal_info(school_login):
-#     info = school_login.get_info()
-#     name = info['real_name']
-#     faculty = info['faculty']
-#     return name, faculty
-
-# 获取指定学年和学期的成绩的函数
-def get_scores(school_login, year, term):
-    return school_login.get_score(score_year=year, score_term=term)
-
-# 将个人信息和成绩格式化为文本字符串的函数
-def format_score_info(scores):
-    info_text = f'相关分数\n'
-    for course in scores:
-        info_text += (
-            f"\n科目: {course['lesson_name']}\n"
-            f"成绩: {course['score']}\n"
-            f"------"
-        )
-    return info_text
-
-def load_key():
-    return open('secret', 'rb').read()
-
-# 使用Fernet进行加密
-def encrypt_message(message):
-    key = load_key()
-    f = Fernet(key)
-    encrypted_message = f.encrypt(message.encode())
-    return encrypted_message
-
-# 使用Fernet进行解密
-def decrypt_message(encrypted_message):
-    key = load_key()
-    f = Fernet(key)
-    decrypted_message = f.decrypt(encrypted_message)
-    return decrypted_message.decode()
-
-# 修改写入文件的函数，使用加密
-def write_to_file(file_path, content, mode='wb'):  # 注意模式改为'wb'因为加密后的数据是字节类型
-    encrypted_content = encrypt_message(content)
-    with open(file_path, mode) as file:
-        file.write(encrypted_content)
-
-# 修改读取文件的函数，使用解密
-def read_file(file_path):
-    with open(file_path, 'rb') as file:  # 注意模式改为'rb'因为加密后的数据是字节类型
-        encrypted_content = file.read()
-    return decrypt_message(encrypted_content)
-
-# 使用PushPlus发送通知的函数
-def send_update_notification(token, message):
-    send_message('成绩', message, token)
-
-# 主函数，负责协调脚本的运行流程
-def main():
-    proxy = random.choice(proxies_list)
-    # 登录教务系统
-    school_login = login_school(URL, USERNAME, PASSWORD, proxy)
-    # 获取个人信息
-    # name, faculty = get_personal_info(school_login)
-    # 获取成绩
-    scores = get_scores(school_login, YEAR, TERM)
-    # 格式化信息和成绩为文本字符串
-    info_text = format_score_info(scores)
-    
-    # 将新数据写入临时文件
-    write_to_file(NEW_DATA_FILE, info_text)
-    
-    # 从数据文件中读取当前的数据
-    source_content = read_file(DATA_FILE)
-    # 从临时文件中读取新的数据
-    new_content = read_file(NEW_DATA_FILE)
-    
-    # 比较当前数据和新数据
-    if source_content != new_content:
-        # 如果数据不同，更新数据文件并发送通知
-        print('推送')
-        write_to_file(DATA_FILE, new_content)
-        send_update_notification(TOKEN, info_text)
-    else:
-        # 如果数据相同，则不需要更新
-        print('不推送')
-    
-    # 在处理完毕后清空临时新数据文件的内容
-    write_to_file(NEW_DATA_FILE, '')
-
-# 该块确保只有在直接运行脚本时才调用main函数
-if __name__ == '__main__':
-    main()
+conf = {
+    "url": URL,
+    "proxies": {"http": "http://117.26.41.218:8888"},
+    "priority_proxy": True  # 值为True时，则直接使用代理访问lan_url地址
+}
+school = SchoolClient(**conf)
+student=school.user_login(USERNAME,PASSWORD,timeout=10)
+schedule_data = student.get_schedule()
+print(schedule_data)
